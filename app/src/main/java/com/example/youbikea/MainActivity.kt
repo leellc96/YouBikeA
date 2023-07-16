2b7f5a0ddbe518bf799208ab0b7c6af80904b7fe
leellc96 <leellc96@gmail.com>
2023-07-16 12:43:18 +0800

    修正UI/UX

--- app/src/main/java/com/example/youbikea/MainActivity.kt
+++ app/src/main/java/com/example/youbikea/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.youbikea
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
+import android.os.Handler
+import android.os.Looper
+import android.text.TextUtils
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -22,49 +28,71 @@ import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
-    private var executorService: ExecutorService? = null
+    lateinit var executorService: ExecutorService
     var GetUbikeStationJsonStr = ""
-    var btn: Button? = null
-    var snaList = ArrayList<String>()
-    var sareaList = ArrayList<String>()
-    var txtMsg: TextView? = null
-    var edtQuery: EditText? = null
+    var snaList: MutableList<String> = ArrayList()
+    var snaListFilter: MutableList<String> = ArrayList()
+    var sareaList: MutableList<String> = ArrayList()
+    var sareaListFilter: MutableList<String> = ArrayList()
+    lateinit var searchView: SearchView
+    private var adapter: RecyclerView.Adapter<*>? = null
+    lateinit var recyclerView: RecyclerView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.btnQuery)
-        edtQuery = findViewById(R.id.edtQueryA)
-        txtMsg = findViewById(R.id.txtMsg)
-        txtMsg!!.setMovementMethod(ScrollingMovementMethod())
-        btn!!.setOnClickListener(View.OnClickListener {
-            txtMsg!!.setText("")
-            StartActAsyncTask()
+        setContentView(R.layout.activity_maina)
+        searchView = findViewById(R.id.search_view)
+        searchView.setIconifiedByDefault(false)
+        searchView.setSubmitButtonEnabled(false)
+        searchView.setQueryHint("搜尋站點")
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        StartActAsyncTask()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (TextUtils.isEmpty(newText)) {
+                    snaListFilter = ArrayList(snaList)
+                    sareaListFilter = ArrayList(sareaList)
+                } else {
+                    sareaListFilter.clear()
+                    snaListFilter.clear()
+                    for (i in sareaList.indices) {
+                        if (sareaList[i].indexOf(newText.trim()) != -1) {
+                            sareaListFilter.add(sareaList[i])
+                            snaListFilter.add(snaList[i])
+                        }
+                    }
+                }
+                adapter = MyAdpater()
+                recyclerView.setAdapter(adapter)
+                return false
+            }
         })
     }
+
     private fun StartActAsyncTask() {
         executorService = Executors.newFixedThreadPool(1)
-        executorService!!.submit(Runnable {
+        executorService.submit(Runnable {
             try {
                 GetUbikeStationJsonStr =
                     QueryUbikeStation(
-          "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+                        "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
                     )
             } catch (e: Exception) {
             }
             doOnUiPost()
         })
     }
+
     private fun doOnUiPost() {
         var i: Int
-        var sna: String
-        var sarea: String
         var StationObject: JSONObject
         val StationArray: JSONArray
-        var str = ""
-        val n: Int
-        val query = edtQuery!!.text.toString()
         if (GetUbikeStationJsonStr != "") {
             try {
                 snaList.clear()
@@ -73,19 +101,18 @@ class MainActivity : AppCompatActivity() {
                 i = 0
                 while (i < StationArray.length()) {
                     StationObject = StationArray.getJSONObject(i)
-                    //snaList.add(StationObject.getString("sna"));
-                    //sareaList.add(StationObject.getString("sarea"));
-                    if (query.trim { it <= ' ' } == StationObject.getString("sarea")) {
-                        str = """$str${StationObject.getString("sarea")}			${
-                            StationObject.getString("sna")
-                        }
-"""
-                    }
+                    snaList.add(StationObject.getString("sna").replace("YouBike2.0_", ""))
+                    sareaList.add(StationObject.getString("sarea"))
                     i++
                 }
-                txtMsg!!.text = str.replace("YouBike2.0_", "")
+                snaListFilter = ArrayList(snaList)
+                sareaListFilter = ArrayList(sareaList)
+                val handler = Handler(Looper.getMainLooper())
+                handler.postDelayed({
+                    adapter = MyAdpater()
+                    recyclerView!!.adapter = adapter
+                }, 100)
             } catch (e: Exception) {
-                n = 0
             }
         }
         try {
@@ -95,6 +122,43 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
         }
     }
+
+    private inner class MyAdpater : RecyclerView.Adapter<MyAdpater.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val menuItemLayoutView = LayoutInflater.from(parent.context).inflate(
+                R.layout.mylistview, parent, false
+            )
+            return ViewHolder(menuItemLayoutView)
+        }
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            if (position % 2 == 0) {
+                holder.layRoot.setBackgroundColor(0xFFFFFFFF.toInt())
+            } else {
+                holder.layRoot.setBackgroundColor(0xFFF0FFF0.toInt())
+            }
+            holder.txtCity.text = "台北市"
+            holder.txtArea.text = sareaListFilter[position]
+            holder.txtTitle.text = snaListFilter[position]
+        }
+
+        override fun getItemCount(): Int {
+            return snaListFilter.size
+        }
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            lateinit var layRoot: LinearLayout
+            var txtCity: TextView
+            var txtArea: TextView
+            var txtTitle: TextView
+
+            init {
+                layRoot = itemView.findViewById(R.id.layRoot)
+                txtCity = itemView.findViewById(R.id.txtCity)
+                txtArea = itemView.findViewById(R.id.txtSarea)
+                txtTitle = itemView.findViewById(R.id.txtSna)
+            }
+        }
+    }
+
     fun QueryUbikeStation(queryString: String?): String {
         var con: HttpURLConnection? = null
         var qResult = ""
@@ -108,22 +172,24 @@ class MainActivity : AppCompatActivity() {
                 url.openConnection() as HttpURLConnection
             }
             con.doInput = true
-            con!!.doOutput = false
+            con?.doOutput = false
             con.connectTimeout = connectTimeout
             con.readTimeout = readTimeout
             val inA: Reader = InputStreamReader(con.inputStream)
             val bufferedreader = BufferedReader(inA)
             val stringBuilder = StringBuilder()
             var stringReadLine: String? = null
-            while (bufferedreader.readLine().also { stringReadLine = it } != null) {
-                stringBuilder.append(stringReadLine)
+            stringReadLine = bufferedreader.readLine()
+            while (stringReadLine != null) {
+                stringBuilder.append(stringReadLine + "\n")
+                stringReadLine = bufferedreader.readLine()
             }
             qResult = stringBuilder.toString()
         } catch (e: IOException) {
             qResult = ""
             e.printStackTrace()
         } finally {
-            con!!.disconnect()
+            con?.disconnect()
         }
         return qResult
     }
